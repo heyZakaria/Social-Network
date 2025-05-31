00e075e81a97d40a7cd00801ed80875414484edf
Mtawil <twlmed212@gmail.com>
2025-05-31 12:09:57 +0100

    Merging with houda

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"path/filepath"
 
 	comment "socialNetwork/comments"
 
@@ -32,13 +31,10 @@ func main() {
 	_, err = db.InitDB("../db/sqlite/database.db")
 
 	router.Handle("/api/", http.StripPrefix("/api", auth.AuthMux()))
-	router.Handle("/rest/", http.StripPrefix("/rest", post.PostMux()))
-	router.Handle("/comment/", http.StripPrefix("/comment", comment.CommentMux()))
-
-	router.HandleFunc("GET /api/verify", middleware.CheckUserExeting)
 
 	router.Handle("/posts/", http.StripPrefix("/posts", post.PostMux()))
 	router.Handle("/likes/", http.StripPrefix("/likes", likes.LikesMux()))
+	router.Handle("/comment/", http.StripPrefix("/comment", comment.CommentMux()))
 
 	// Profile routes
 	router.HandleFunc("GET /api/users/profile", profile.GetUserProfile)
@@ -53,17 +49,5 @@ func main() {
 	router.HandleFunc("POST /api/users/reject", profile.RejectFollowRequest)
 	//  router.HandleFunc("GET /api/users/suggestions", profile.GetUserSuggestions)
 
-	// Testing serving images
-	//  http://localhost:8080/uploads/posts/40809c81-b8b6-45aa-8311-4abe9de995f8.JPEG
-	router.HandleFunc("/uploads/", func(w http.ResponseWriter, r *http.Request) {
-		// get Path and file name
-		filename := filepath.Base(r.URL.Path)  // 40809c81-b8b6-45aa-8311-4abe9de995f8.JPEG
-		PathFolder := filepath.Dir(r.URL.Path) // uploads/posts/
-
-		// concat Paths
-		fullPath := filepath.Join("../"+PathFolder, filename)
-		// Serve the image to the user
-		http.ServeFile(w, r, fullPath)
-	})
 	log.Fatal(http.ListenAndServe(":8080", middleware.CheckCORS(middleware.CheckUserExeting(router))))
 }
