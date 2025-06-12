9b634fd763beaa3baed216e1d8c8d6763298b0c5
Mtawil <twlmed212@gmail.com>
2025-06-12 15:24:35 +0100

    Restore Session Logic And Token Create instead of JWT

--- backend/auth/cookies.go
+++ backend/auth/cookies.go
@@ -4,44 +4,32 @@ import (
 	"net/http"
 	"time"
 
+	tkn "socialNetwork/token"
 	"socialNetwork/utils"
+
+	"github.com/google/uuid"
 )
 
 func SendSuccessWithToken(w http.ResponseWriter, r *http.Request, userID string) {
-	// second parm not necessary "user", just for respect format of JWT
-	// Sould be choose role of user
-	token, err := CreateJWT(userID, "user")
-	if err != nil {
-		utils.Log("ERROR", "Failed to create JWT: "+err.Error())
-		utils.SendJSON(w, http.StatusBadRequest, utils.JSONResponse{
-			Success: false,
-			Error:   "Internal server error",
-		})
-		return
-	}
-	// set cookies manual like we recieve it in graphql
-	// w.Header().Set("Authorization", "Bearer "+token)
 
-	// set cookies auto -------> show file of cookies in Browser to understand this part
-	// secure vs XSS
+	token := uuid.New().String() // TODO Looking for BEST Way to generate the Session ID :D
+
 	http.SetCookie(w, &http.Cookie{
 		Name:     "token",
 		Value:    token,
 		Path:     "/",
 		HttpOnly: true,
 
-		// should be true if we work by https
-		// to use https should be install SSL ----> sudo apt install openssl (Linux)
-		// openssl req -newkey rsa:2048 -nodes -keyout server.key -out server.crt
-
-		// TO run server use SSL
-		// 	err = http.ListenAndServeTLS(":8080", "server.crt", "server.key", nil)
 		Secure: false,
 
 		// secure vs CSRF attacks (Cross Site Request Forgery)
 		SameSite: http.SameSiteStrictMode,
 		Expires:  time.Now().Add(time.Hour * 24),
 	})
+
+	utils.Log("INFO", "Save Token into Sessions")
+	tkn.SaveToken(userID, token)
+
 	utils.SendJSON(w, http.StatusOK, utils.JSONResponse{
 		Success: true,
 		Message: "Login successful",
--- backend/auth/model.go
+++ backend/auth/model.go
@@ -4,8 +4,6 @@ import (
 	"time"
 )
 
-var secretKey = []byte("Y&8vBx2!r4@LmP7qZpN1*Eg5Km%VcX0t")
-
 type Profile struct {
 	UserID         string    `json:"id"`
 	FirstName      string    `json:"firstname"`
@@ -18,12 +16,6 @@ type Profile struct {
 	Profile_Status string    `json:"profile_status"`
 	Birthday       time.Time `json:"birthday"`
 	CreatedAt      time.Time
-}
-
-type JWTPayload struct {
-	UserID string `json:"user_id"`
-	Role   string `json:"role"`
-	Exp    int64  `json:"exp"`
 }
 
 type LoginRequest struct {
